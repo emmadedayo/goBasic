597ea9ece0e4b4455485983119f6ccab75684765
Adenagbe Emmanuel <emmadenagbe@gmail.com>
2022-08-24 13:34:00 +0100

    Declaring variables

---
+++ variables/variable.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Using string
+	var variable string = "i'm Emmanuel Adenagbe"
+	fmt.Println(variable)
+
+	var variable2 int = 10
+	fmt.Println(variable2)
+
+	var variable3 bool = true
+	fmt.Println(variable3)
+
+	var variable4 float32 = 10.5
+	fmt.Println(variable4)
+
+	var variable5 rune = 'a'
+	fmt.Println(variable5)
+
+	var variable6 complex64 = 10 + 10i
+	fmt.Println(variable6)
+
+	var variable7 complex128 = 10 + 10i
+	fmt.Println(variable7)
+
+	var variable8 uint = 10
+	fmt.Println(variable8)
+
+	var variable9 uint8 = 10
+	fmt.Println(variable9)
+
+	var variable10 uint16 = 10
+	fmt.Println(variable10)
+
+	var variable11 uint32 = 10
+	fmt.Println(variable11)
+
+	var variable12 uint64 = 10
+	fmt.Println(variable12)
+
+	var variable13 byte = 10
+	fmt.Println(variable13)
+
+	var variable14 rune = 10
+	fmt.Println(variable14)
+
+}
